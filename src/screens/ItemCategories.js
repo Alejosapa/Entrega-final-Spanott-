b844fddcd020d05aa779db66a5130e096771311a
Alejosapa <alejosapanott2@gmail.com>
2024-09-11 16:17:06 -0300

    agrgando carrito

--- src/screens/ItemCategories.js
+++ src/screens/ItemCategories.js
@@ -1,11 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
-import Header from '../components/Header'
 import Categories from '../components/Categories'
 
 const ItemCategories = () => {
   return (
     <View>
-      
     <Categories />
     </View>
   )
